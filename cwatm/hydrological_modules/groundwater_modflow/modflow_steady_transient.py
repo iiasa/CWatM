cafbad6c7b8ab01fccde88c86e440bf30fc5572e
Mikhail Misha Smilovic <mikhail.smilovic@gmail.com>
2020-04-14 12:29:15 +0200

    Improved demand2pumping option
    Groundwater demand is now distributed to all underlying MODFLOW cells, and the package pyproj is no longer necessary.

--- cwatm/hydrological_modules/groundwater_modflow/modflow_steady_transient.py
+++ cwatm/hydrological_modules/groundwater_modflow/modflow_steady_transient.py
@@ -122,82 +122,25 @@ def modflow_transient(self,compteur):
     self.var.Volume_modflow = np.nansum(gwrecharge * self.var.res_ModFlow * self.var.res_ModFlow)
     gwrecharge = gwrecharge.reshape(domain['nrow'], domain['ncol'])
 
-    #ProjMeanRecharge(res_ModFlow, res_CWATM, Inputs_file, InitRecharge, Outputs_file + 'GWrecharge_map.txt')
-
-    # To Modflow subroutine
-    ### MODIF LUCA, add the pumping case
-
-    wgs84 = pyproj.Proj("+init=EPSG:4326")  # Associated coord system
-    UTM = pyproj.Proj("+init=EPSG:32643")  # Projection system for the Upper Bhima basin
-
-    self.var.lats = loadmap('Lat')
-    self.var.lons = loadmap('Lon')
-
-    toNPY = [self.var.modflowPumping, self.var.lats, self.var.lons]
-    # print(toNPY)
-
-    pumping = toNPY[0]
-    lat = toNPY[1]
-    lon = toNPY[2]
-
     if self.var.GW_pumping:
+        if checkOption('demand2pumping'):
 
-        if 'demand2pumping' in binding:
-            if cbinding('demand2pumping') == 'True':
-                demand2pumping = True
-            else:
-                demand2pumping = False
+            ## INSTALLING WELLS
+            ## Groundwater demand from CWatM installs wells in each Modflow cell
+
+            # Groundwater pumping demand from the CWatM waterdemand module, will be decompressed to 2D array
+            # using the sumed up (over e.g 7 days groundwater pumping demand)
+            pump1 = decompress(self.var.modflowPumpingM)
+            # CWatM 2D groundwater pumping array is converted into Modflow 2D array
+            pumping = indexes['Weight'] * pump1[indexes['CWATMindex']] * self.var.res_ModFlow * self.var.res_ModFlow
+
+            pumping = pumping.reshape(domain['nrow'], domain['ncol'])
+            Wells = np.array(
+                [[row, col, -pumping[row, col]] for col in range(len(pumping[0, :])) for row in range(len(pumping[:, 0])) if
+                 pumping[row, col] > 0])
         else:
-            demand2pumping = False
-
-        if demand2pumping == False:
-            pumping_data = cbinding('Pumping_input_file')
-            pumping_data = np.load(pumping_data)
-
-        else:
-
-            ## CONVERSION OF THE COORDINATES into UTM ##
-
-            x = []
-            y = []
-
-            Coord = pyproj.transform(wgs84, UTM, self.var.lons, self.var.lats)
-
-            x = Coord[0]
-            y = Coord[1]
-
-
-            ##Conversion into indices
-            #f = open('UB_limits.txt')
-            f = open(cbinding('catchment_limits'))
-            lines = list(f)
-
-            min_x = float(lines[0]) // 1
-            max_y = float(lines[2]) // 1
-
-            xi = []
-            yi = []
-
-            for i in range(len(lat)):
-                xi.append(int((x[i] - min_x) // 500))
-                yi.append(int((max_y - y[i]) // 500))
-
-            Wells = np.array([[yi[i], xi[i], -pumping[i]] for i in range(len(lat))])  # *-10 to get exaggerated effect
-            # print(Wells)
-            print(len(Wells))
-            #np.save('Pumping_input_file', Wells)
-            # Reset 7-day pumping amount
-            self.var.modflowPumping = globals.inZero.copy()
-
-            #pumping_data_file = cbinding('Pumping_input_file')
-            #pumpage = np.load(pumping_data_file)
-
-            # leakage = np.load('Pumping_input_file_10000.npy')
-            # leakage = np.load('Pumping_input_file_900000.npy')
-
-            #pumping_data = pumpage  # np.concatenate((pumpage, leakage))
-            #print(len(pumping_data))
-            #np.save('Pumping_input_file', pumping_data)
+            Wells = []
+	
 
         cap,base, budget_terms = ModFlow_modelV5(self,self.var.PathModflow , compteur, 1, self.var.modflow_timestep, domain['nrow'],domain['ncol'], gwrecharge, pumping_datas=Wells)
     else:
