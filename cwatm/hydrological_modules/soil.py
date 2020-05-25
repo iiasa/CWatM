dcd1ce77c97cba4534771e9c88c568f07bc7c857
CWatM <peter.burek@gmx.de>
2020-05-25 16:52:54 +0200

    add: addition self.var description to soil as test

--- cwatm/hydrological_modules/soil.py
+++ cwatm/hydrological_modules/soil.py
@@ -31,6 +31,18 @@ class soil(object):
 
         * Initialize all the hydraulic properties of soil
         * Set soil depth
+        
+        **Global variables**
+        * **self.var.soilLayers** Number of soil layers Unit: - Appears: soil
+        * **self.var.percolationImp** Fraction of area where percolation to groundwater is impeded    Unit: -       Appears: soil, waterdemand
+        
+        =======================  =============================================  =======  ===========
+        Variable [self.var]      Description                                    Unit     Appears in
+        =======================  =============================================  =======  ===========
+        soilLayers               Number of soil layers Unit                     -        soil
+        percolationImp           Fraction of area where percolation to groundwater is impeded  -  soil,waterdemand
+        =======================  =============================================  =======  ===========
+
         """
 
         self.var.soilLayers = 3
