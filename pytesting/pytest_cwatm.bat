9026c288dcfdbc72c17613f17cb21910b6334ef4
CWatM <peter.burek@gmx.de>
2020-12-11 15:51:54 +0100

    chg: setup travis, codecov and uploaded a pytest coverage.xml

--- pytesting/pytest_cwatm.bat
+++ pytesting/pytest_cwatm.bat
@@ -1,5 +1,8 @@
-coverage run -m pytest test_cwatm3.py --html=report222.html --settingsfile=test_py_cwatm2.txt --cwatm=C:/work/CWATM/run_cwatm.py
-coverage html
+rem coverage run -m pytest test_cwatm3.py --html=report111220.html --settingsfile=test_py_cwatm2.txt --cwatm=C:/work/CWATM/run_cwatm.py
+rem coverage html
+
+pytest test_cwatm3.py --cov-report=xml --cov=cwatm tests/  --html=report111220.html --settingsfile=test_py_cwatm2.txt --cwatm=C:/work/CWATM/run_cwatm.py
+
 pause
 
 rem https://coverage.readthedocs.io/en/coverage-5.3/
