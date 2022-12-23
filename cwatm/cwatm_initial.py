3d595cc86f594dadef308870059feed120cbfb88
Dor Fridman <dof1985@gmail.com>
2022-12-23 13:57:42 +0100

    response to commit #53
    removed  #test4real from line 10 of #53

--- cwatm/cwatm_initial.py
+++ cwatm/cwatm_initial.py
@@ -7,7 +7,6 @@
 # Created:     16/05/2016
 # Copyright:   (c) PB 2016
 # -------------------------------------------------------------------------
-# test4real
 
 from cwatm.hydrological_modules.miscInitial import miscInitial
 from cwatm.hydrological_modules.initcondition import initcondition
