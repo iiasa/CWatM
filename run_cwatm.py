334c648aa9f2deddae5e64f6822cd06b5f381b7e
Peter Burek <burek@iiasa.ac.at>
2024-10-29 14:29:36 +0100

    updated run_cwatm.py and an example settingsfile

--- run_cwatm.py
+++ run_cwatm.py
@@ -33,7 +33,7 @@ import sys
 
 from cwatm import __author__, __version__, __date__, __copyright__, __maintainer__, __status__
 from cwatm.run_cwatm import main, mainwarm, parse_args, usage, GNU
-# mainwarm for calibration
+
 if __name__ == "__main__":
     settings, args = parse_args()
     main(settings, args)
